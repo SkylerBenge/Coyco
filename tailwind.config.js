53142b9bc1a8d9b805a41bfa03709f29e81c4fb6
SkylerBenge <skycbenge@gmail.com>
2021-04-25 12:59:20 -0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ["./**/*.liquid"]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
