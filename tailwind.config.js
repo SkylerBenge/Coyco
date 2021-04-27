413cf342c8f484df28992cdc3c6e20ba1c8b0151
SkylerBenge <skycbenge@gmail.com>
2021-04-27 16:57:57 -0700

    homepage desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,57 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: {
+        DEFAULT: '#222222'
+      },
+      red: {
+        DEFAULT: '#a1342d'
+      },
+      yellow: {
+        DEFAULT: '#ff9b25'
+      },
+      cream: {
+        DEFAULT: '#f5f5e5'
+      }
+    },
+    fontFamily: {
+      'bungee': ['Bungee', 'cursive'],
+      'bungee-inline': ['Bungee Inline', 'cursive']
+    },
+    keyframes: {
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-12deg)' },
+        '50%': { transform: 'rotate(12deg)' },
+      },
+      bounce: {
+        '0%, 100%': {
+          transform: 'translateY(-1rem)',
+          animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+        },
+        '50%': {
+          transform: 'translateY(0)',
+          animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
+         }
+      }
+    },
+    animation: {
+      wiggle: 'wiggle 6s ease-in-out infinite',
+      bounce: 'bounce 3s infinite',
+    },
+    zIndex: {
+      '-1': '-1',
+      '1': '1',
+      '2': '2',
+      '3': '3',
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['hover', 'focus']
+    },
   },
   plugins: [],
 }
